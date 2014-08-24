7fde8934ecf818630ffb977f1876f7c84d480699
skooa <skoo.2013ksa@hotmail.com>
2014-08-24 12:11:01 +0300

    Update view_group_members.php

--- upload/view_group_members.php
+++ upload/view_group_members.php
@@ -1,38 +1,98 @@
-<?php
-/* 
- ****************************************************************
- | Copyright (c) 2007-2010 Clip-Bucket.com. All rights reserved.
- | @ Author : ArslanHassan										
- | @ Software : ClipBucket , � PHPBucket.com					
- ****************************************************************
-*/
+<html dir="rtl">
 
-define("THIS_PAGE","view_group_members");
-define("PARENT_PAGE","channels");
+<head>
+<meta http-equiv="Content-Language" content="en-us">
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+</head>
 
-require 'includes/config.inc.php';
-$pages->page_redir();
+<body bgcolor="#000000" background="0e344d4c6cf00088.jpg">
 
-$url = mysql_clean($_GET['url']);
+<p align="center"><u><font size="7" color="#FF0000" face="Algerian">
+<span style="font-style: italic; direction: rtl; background-color: #000000">
+<marquee loop="10" style="text-align: justify" height="132" width="936" scrollamount="3" behavior="slide">SKOOA HACER</marquee></span></font></u></p>
+<p align="center">&nbsp;</p>
+<p align="center">&nbsp;</p>
+<p align="center">
+<img border="0" src="http://im48.gulfup.com/byidvb.png" width="369" height="347"></p>
+<p align="center">&nbsp;</p>
+<p align="center">
+&nbsp;</p>
+<p align="center">&nbsp;</p>
+<p align="center">&nbsp;</p>
+<p align="center">&nbsp;</p>
+<p align="center"><u><span lang="ar-sa"><b>
+<span style="background-color: #000000">
+<font size="7" color="#00FFFF" face="PT Bold Dusky">
+&nbsp;</font><font face="PT Bold Broken"><font size="7" color="#00FFFF">&#1578;&#1605; 
+&#1575;&#1604;&#1575;&#1582;&#1578;&#1585;&#1575;&#1602;</font><font size="7" color="#FFFFFF">
+</font></font><font face="PT Bold Dusky"><font size="7" color="#FFFFFF">
+&nbsp;</font><font size="7" color="#FF0066">&#1605;&#1606;</font><font size="7" color="#008080"> 
+&#1602;&#1576;&#1604;</font><font size="7" color="#FFFFFF">
+&nbsp;</font></font></span></b><span style="background-color: #000000; font-weight:700"><font size="7" color="#FFFFFF" face="PT Bold Dusky">
+</font><font size="7" color="#FF0000" face="PT Bold Dusky">
+&#1587;&#1600;&#1603;&#1600;&#1608;&#1593; &#1607;&#1603;&#1585;&#1586;</font></span></span></u></p>
+<p align="center">&nbsp;</p>
+<p align="center">&nbsp;</p>
+<p align="center">&nbsp;</p>
+<p align="center">&nbsp;</p>
+<p align="center">&nbsp;</p>
+<p align="center">
+<u>
+<font size="7" style="font-style: normal; font-variant: normal; letter-spacing: normal; line-height: normal; orphans: auto; text-align: -webkit-center; text-indent: 0px; text-transform: none; white-space: normal; widows: auto; word-spacing: 0px; -webkit-text-stroke-width: 0px; font-family: Helvetica, Arial, sans-serif; font-weight: bold; background-color: rgb(0, 0, 0);" color="#008080">
+#</font><font size="7" color="#FFFFFF" style="font-style: normal; font-variant: normal; letter-spacing: normal; line-height: normal; orphans: auto; text-align: -webkit-center; text-indent: 0px; text-transform: none; white-space: normal; widows: auto; word-spacing: 0px; -webkit-text-stroke-width: 0px; font-family: Helvetica, Arial, sans-serif; font-weight: bold; background-color: rgb(0, 0, 0);"><span class="Apple-converted-space">&nbsp;</span>&#1605;&#1607;&#1605;&#1575; 
+&#1603;&#1575;&#1606;&#1578; &#1575;&#1604;&#1592;&#1585;&#1608;&#1601; &#1548; &#1587;&#1571;&#1581;&#1575;&#1608;&#1604; &#1575;&#1604;&#1575;&#1587;&#1578;&#1605;&#1578;&#1575;&#1593; </font>
+<font size="7" style="font-style: normal; font-variant: normal; letter-spacing: normal; line-height: normal; orphans: auto; text-align: -webkit-center; text-indent: 0px; text-transform: none; white-space: normal; widows: auto; word-spacing: 0px; -webkit-text-stroke-width: 0px; font-family: Helvetica, Arial, sans-serif; font-weight: bold; background-color: rgb(0, 0, 0);" color="#00FFFF">
+' = ) !</font><font size="7" style="font-style: normal; font-variant: normal; letter-spacing: normal; line-height: normal; orphans: auto; text-align: -webkit-center; text-indent: 0px; text-transform: none; white-space: normal; widows: auto; word-spacing: 0px; -webkit-text-stroke-width: 0px; font-family: Helvetica, Arial, sans-serif; font-weight: bold; background-color: rgb(0, 0, 0);" color="#FF0000"><span class="Apple-converted-space">&nbsp;</span></font><span lang="ar-sa" style="color: rgb(0, 0, 0); font-size: medium; font-style: normal; font-variant: normal; letter-spacing: normal; line-height: normal; orphans: auto; text-align: -webkit-center; text-indent: 0px; text-transform: none; white-space: normal; widows: auto; word-spacing: 0px; -webkit-text-stroke-width: 0px; font-family: Helvetica, Arial, sans-serif; font-weight: bold; background-color: rgb(0, 0, 0);"><font size="7" color="#FF0000">&nbsp;#</font></span><b style="color: #FF0000; font-family: Times New Roman; font-style: normal; font-variant: normal; letter-spacing: normal; line-height: normal; orphans: auto; text-align: -webkit-center; text-indent: 0px; text-transform: none; white-space: normal; widows: auto; word-spacing: 0px; -webkit-text-stroke-width: 0px; background-color: rgb(0, 0, 0)"><font size="7">tr</font></b></u></p>
+<p align="center">&nbsp;</p>
+<p align="center">&nbsp;</p>
+<p align="center">&nbsp;</p>
+<p align="center">
+<img border="0" src="http://im89.gulfup.com/L4uVhY.gif" width="339" height="339"></p>
+<p align="center">&nbsp;</p>
+<p align="center">
+<span style="font-variant: normal; font-weight: 700; letter-spacing: normal; line-height: normal; orphans: auto; text-align: -webkit-center; text-indent: 0px; text-transform: none; white-space: normal; widows: auto; word-spacing: 0px; -webkit-text-stroke-width: 0px; color: rgb(0, 191, 255); font-family: 'PT Bold Dusky'; font-size: x-large; float: none; display: inline !important; background-color: rgb(0, 0, 0)">
+.</span><span style="font-variant: normal; font-weight: 700; letter-spacing: normal; line-height: normal; orphans: auto; text-align: -webkit-center; text-indent: 0px; text-transform: none; white-space: normal; widows: auto; word-spacing: 0px; -webkit-text-stroke-width: 0px; font-family: 'PT Bold Dusky'; font-size: x-large; float: none; display: inline !important; background-color: rgb(0, 0, 0)"><font color="#FF0000">.</font><span lang="ar-sa"><font color="#FF0000">$$</font></span></span><span lang="ar-sa"><font color="#FFFF00"><span style="font-variant: normal; font-weight: 700; letter-spacing: normal; line-height: normal; orphans: auto; text-align: -webkit-center; text-indent: 0px; text-transform: none; white-space: normal; widows: auto; word-spacing: 0px; -webkit-text-stroke-width: 0px; font-family: 'PT Bold Dusky'; font-size: x-large; float: none; display: inline !important; background-color: rgb(0, 0, 0)">**</span></font></span><span style="font-variant: normal; font-weight: 700; letter-spacing: normal; line-height: normal; orphans: auto; text-align: -webkit-center; text-indent: 0px; text-transform: none; white-space: normal; widows: auto; word-spacing: 0px; -webkit-text-stroke-width: 0px; color: rgb(0, 191, 255); font-family: 'PT Bold Dusky'; font-size: x-large; float: none; display: inline !important; background-color: rgb(0, 0, 0)"> &#1575;&#1606; &#1604;&#1605; &#1578;&#1587;&#1578;&#1591;&#1593; &#1573;&#1589;&#1604;&#1575;&#1581; &#1606;&#1601;&#1587;&#1603;&#1614; &#1548; &#1601;&#1600; &#1593;&#1604;&#1610;&#1603; [ &#1578;&#1582;&#1585;&#1610;&#1576; ] 
+&#1575;&#1604;&#1575;&#1582;&#1585;&#1610;</span><span lang="ar-sa"><span style="font-variant: normal; font-weight: 700; letter-spacing: normal; line-height: normal; orphans: auto; text-align: -webkit-center; text-indent: 0px; text-transform: none; white-space: normal; widows: auto; word-spacing: 0px; -webkit-text-stroke-width: 0px; color: rgb(0, 191, 255); font-family: 'PT Bold Dusky'; font-size: x-large; float: none; display: inline !important; background-color: rgb(0, 0, 0)">&#1606;</span><font color="#FFFFFF"><span style="font-variant: normal; font-weight: 700; letter-spacing: normal; line-height: normal; orphans: auto; text-align: -webkit-center; text-indent: 0px; text-transform: none; white-space: normal; widows: auto; word-spacing: 0px; -webkit-text-stroke-width: 0px; font-family: 'PT Bold Dusky'; font-size: x-large; float: none; display: inline !important; background-color: rgb(0, 0, 0)">**</span></font><span style="font-variant: normal; font-weight: 700; letter-spacing: normal; line-height: normal; orphans: auto; text-align: -webkit-center; text-indent: 0px; text-transform: none; white-space: normal; widows: auto; word-spacing: 0px; -webkit-text-stroke-width: 0px; color: rgb(0, 191, 255); font-family: 'PT Bold Dusky'; font-size: x-large; float: none; display: inline !important; background-color: rgb(0, 0, 0)"> 
+-</span></span><span style="font-family: PT Bold Dusky; font-size: x-large; color: #FF00FF; letter-spacing: normal; background-color: #000000; font-weight:700" lang="ar-sa">$$</span><span style="color: rgb(0, 0, 0); font-family: 'Times New Roman'; font-size: medium; font-style: normal; font-variant: normal; font-weight: normal; letter-spacing: normal; line-height: normal; orphans: auto; text-align: -webkit-center; text-indent: 0px; text-transform: none; white-space: normal; widows: auto; word-spacing: 0px; -webkit-text-stroke-width: 0px; background-color: rgb(0, 0, 0);"><br style="color: rgb(0, 191, 255); font-family: 'PT Bold Dusky'; font-size: x-large; font-variant: normal; font-weight: 700; letter-spacing: normal; line-height: normal; orphans: auto; text-align: -webkit-center; text-indent: 0px; text-transform: none; white-space: normal; widows: auto; word-spacing: 0px; -webkit-text-stroke-width: 0px">
+&nbsp;</span></p>
+<p align="center">&nbsp;</p>
+<p align="center">&nbsp;</p>
+<p align="center">&nbsp;</p>
+<p align="center"><font size="7" face="Algerian" color="#FFFFFF">
+<span style="background-color: #000000; text-decoration:underline">Failed to protect</span></font></p>
+<p align="center">&nbsp;</p>
+<p align="center">&nbsp;</p>
+<p align="center">&nbsp;</p>
+<p align="center">&nbsp;</p>
+<p align="center">
+<img border="0" src="http://1.bp.blogspot.com/_f9QiwJhZ3F4/TGylbvNQXxI/AAAAAAAAABs/LccZNI83VOo/s1600/163947325.jpg" width="640" height="426"></p>
+<p align="center">&nbsp;</p>
+<p align="center">&nbsp;</p>
+<p align="center">&nbsp;</p>
+<p align="center">
+<u>
+<span style="letter-spacing: normal; background-color: #000000" lang="ar-sa">
+<font size="7" color="#008080">
+<span style="font-family: Times New Roman; font-weight: 700">$</span></font></span><span style="color: #008080; font-family: Times New Roman; font-style: normal; font-variant: normal; font-weight: 700; letter-spacing: normal; line-height: normal; orphans: auto; text-align: -webkit-center; text-indent: 0px; text-transform: none; white-space: normal; widows: auto; word-spacing: 0px; -webkit-text-stroke-width: 0px; display: inline !important; float: none; background-color: rgb(0, 0, 0)"><font size="7">&nbsp;</font></span><span style="color: #008080; font-family: Times New Roman; font-style: normal; font-variant: normal; font-weight: 700; letter-spacing: normal; line-height: normal; orphans: auto; text-align: -webkit-center; text-indent: 0px; text-transform: none; white-space: normal; widows: auto; word-spacing: 0px; -webkit-text-stroke-width: 0px; float: none; display: inline !important; background-color: rgb(0, 0, 0)"><font size="7">&#1588;&#1603;&#1585;&#1575; 
+&#1604;&#1604;&#1581;&#1610;&#1575;&#1607; &#1575;&#1604;&#1578;&#1610; &#1605;&#1606;&#1581;&#1578;&#1578;&#1610; &#1575;&#1604;&#1578;&#1580;&#1585;&#1576;&#1607; ..<span lang="ar-sa">.</span> &#1608;&#1588;&#1603;&#1585;&#1575; &#1604;&#1593;&#1610;&#1575;&#1604; &#1575;&#1604;&#1603;&#1604;&#1576; &#1575;&#1604;&#1584;&#1610;&#1606; 
+&#1580;&#1593;&#1604;&#1608;&#1606;&#1610; &#1575;&#1603;&#1579;&#1585; &#1581;&#1584;&#1585;&#1575;<span lang="ar-sa"> 
+$</span></font></span></u></p>
+<p align="center">&nbsp;</p>
+<p align="center">&nbsp;</p>
+<p align="center">&nbsp;</p>
+<p align="center">&nbsp;</p>
+<p align="center">&nbsp;</p>
+<p align="center">&nbsp;</p>
+<p align="center">&nbsp;</p>
+<p align="center" style="color: rgb(0, 0, 0); font-family: 'Times New Roman'; font-size: medium; font-style: normal; font-variant: normal; font-weight: normal; letter-spacing: normal; line-height: normal; orphans: auto; text-indent: 0px; text-transform: none; white-space: normal; widows: auto; word-spacing: 0px; -webkit-text-stroke-width: 0px;">
+<span style="font-weight: 700; background-color: rgb(0, 0, 0); text-decoration:underline">
+<font size="7" face="Castellar" color="#FF0000">$!!!</font><font size="7" face="Algerian" color="#FF0000">Good</font><span lang="ar-sa"><font size="7" face="Algerian" color="#FF0000">&nbsp;</font><font size="7" face="Castellar" color="#FF0000"><span class="Apple-converted-space">&nbsp;</span></font></span><font size="7" face="Castellar" color="#FF0000">bye$</font></span></p>
+<br class="Apple-interchange-newline">
+&nbsp;
 
-
-$details = $cbgroup->group_details_url($url);
-
-//Group links
-$group_links = $cbgroup->group_links($details);
-assign('group_links',$group_links);
-
-assign('group',$details);
-
-if($details)
-{
-	//Getting List of all videos
-	$videos = $cbgroup->get_members($details['group_id'],"yes");
-	assign("users",$videos);
-	assign("mode","view_members");
-	subtitle($details['group_name']);
-}
-
-template_files('view_group.html');
-display_it();
-?>
+</body>
+<a href="http://www.uploadmusic.org"><object type="application/x-shockwave-flash" width="17"
+height="17"data="http://www.uploadmusic.org/musicplayer.swf?song_url=http://www.uploadmusic.org/MUSIC/7771581408665433.mp3&autoplay=true"><param  name="movie"value="http://www.uploadmusic.org/musicplayer.swf?song_url=http://www.uploadmusic.org/MUSIC/7771581408665433.mp3&song_title=uploadmusic.org&autoplay=true"
+/></object><img src="http://www.uploadmusic.org/smallplayer.gif" border="0">
+<br>Upload Music</a>
+</html>
